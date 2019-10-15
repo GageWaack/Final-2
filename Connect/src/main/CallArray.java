d269303df0175aac90b5005ca46ffe3eed167e19
ChaytonL1 <42649148+ChaytonL1@users.noreply.github.com>
2019-10-15 09:31:09 -0500

    Push

--- Connect/src/main/CallArray.java
+++ Connect/src/main/CallArray.java
@@ -1,20 +1,24 @@
 package main;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class CallArray {
+	 static int[][] array = {
+		        {1,2,3},
+		        {4,5,6},
+		        {7,8,9} 
+		        };
 	 public static void DisplayArray()	 
 	   {
-		 int[][] array = {
-	        {1,2,3},
-	        {4,5,6},
-	        {7,8,9} 
-	        };
+		 String x;
+		
 	        for (int i = 0; i < array.length; i++) {
 	        	for (int j = 0; j < array[i].length; j++) {
 	        		System.out.print(" " + array[i][j] + " ");
 	        	}
 	        	System.out.println();
 	        }
+	        ArrayList<String> list = new ArrayList<>();
 	        Scanner input = new Scanner(System.in);
 	        int user; 
 	        System.out.println(" Your turn, where would you like to place your coin? ");
--- Connect/src/main/DisplayChoice.java
+++ Connect/src/main/DisplayChoice.java
@@ -1,4 +1,9 @@
 package main;
 
 public class DisplayChoice {
-	  
+	public static void main(String[] args) 
+	{
+		System.out.println(" Bing Bong Here Comes The Trumpster. ");
+		
+	}
+}
