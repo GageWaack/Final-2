5daa3b836dedfd54bd4e1bc3c7b911ddab9cae7d
ChaytonL1 <42649148+ChaytonL1@users.noreply.github.com>
2019-10-15 09:14:23 -0500

    Testing Transfer

--- Connect/src/main/CallArray.java
+++ Connect/src/main/CallArray.java
@@ -1,9 +1,10 @@
 package main;
+import java.util.Scanner;
 
 public class CallArray {
-	 public static void DisplayArray()
-	    {
-	        int[][] array = {
+	 public static void DisplayArray()	 
+	   {
+		 int[][] array = {
 	        {1,2,3},
 	        {4,5,6},
 	        {7,8,9} 
@@ -14,5 +15,53 @@ public class CallArray {
 	        	}
 	        	System.out.println();
 	        }
+	        Scanner input = new Scanner(System.in);
+	        int user; 
+	        System.out.println(" Your turn, where would you like to place your coin? ");
+	        user = input.nextInt();
+	        if (user == 1 ) 
+	        {
+	        	array[0][0] = 'x';
+	        }
+	        if (user == 2 ) 
+	        {
+	        	array[0][1] = 'x';
+	        }
+	        if (user == 3 ) 
+	        {
+	        	array[0][2] = 'x';
+	        }
+	        if (user == 4 ) 
+	        {
+	        	array[1][0] = 'x';
+	        }
+	        if (user == 5 ) 
+	        {
+	        	array[1][1] = 'x';
+	        }
+	        if (user == 6 ) 
+	        {
+	        	array[1][2] = 'x';
+	        }
+	        if (user == 7 ) 
+	        {
+	        	array[2][0] = 'x';
+	        }
+	        if (user == 8 ) 
+	        {
+	        	array[2][1] = 'x';
+	        }
+	        if (user == 9 ) 
+	        {
+	        	array[2][2] = 'x';
+	        }
+
+	        for (int i = 0; i < array.length; i++) {
+	        	for (int j = 0; j < array[i].length; j++) {
+	        		System.out.print(" " + array[i][j] + " ");
+	        	}
+	        	System.out.println();
+	    
 	    }
+	   }
 }
--- Connect/src/main/Main.java
+++ Connect/src/main/Main.java
@@ -1,10 +1,11 @@
 package main;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			CallArray.DisplayArray();
-	}
-
+			
+	}	
 }
