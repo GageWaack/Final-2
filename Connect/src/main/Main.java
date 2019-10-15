b226dc19f80fe17c9e1bbffd556eaedf5805958d
GageWaack <45102148+GageWaack@users.noreply.github.com>
2019-10-15 09:17:47 -0500

    update

--- Connect/src/main/Main.java
+++ Connect/src/main/Main.java
@@ -1,6 +1,19 @@
-package main;
+import java.util.Scanner;
 
 public class Main {
 
-    //this is the main class for the project
+    public static void main (String[]args)
+    {
+//        int ComputerInput;
+//        Scanner UserInput = new Scanner (System.in);
+//        char User;
+//        System.out.print("Hello, do you want to be X's or O's");
+       // User = UserInput.nextLine();
+
+       // if (User == X)
+//        {
+//
+//        }
+    	PlainBoard.displayBetterBoard();
+    }
 }
