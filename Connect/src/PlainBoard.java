b6b626e92dde1c80f2c9f41241ae3c6645c0a9af
GageWaack <45102148+GageWaack@users.noreply.github.com>
2019-10-16 09:49:26 -0500

    update

--- Connect/src/PlainBoard.java
+++ Connect/src/PlainBoard.java
@@ -17,7 +17,7 @@ public class PlainBoard
 		}
 		for (int j = 0; j < 15; ++j ) 
 		{
-		System.out.println("|	  			 |			    X			|				|");
+		System.out.println("|	  			 |			    			|				|");
 		}
 		for (int i = 0; i < 113; ++i) 
 		{
@@ -32,7 +32,31 @@ public class PlainBoard
 			System.out.print("_");
 		}				
 		
-			
+		System.out.println(" ");
+    	int[] a = new int[9];
+        a[0] = a[5] = a[6] = 10;
+        a[3] = a[7] = 100;
+        a[1] = a[2] = a[4] = a[8] = 0;
+        String[] b = new String[9];
+        int x = 0;
+
+        while (x < a.length) {
+            if (a[x] == 10)
+                b[x] = "X";
+            else if (a[x] == 100)
+                b[x] = "O";
+            else if (a[x] == 0)
+                b[x] = " ";
+            x++;
+        }
+        System.out.println("+---+---+---+");
+        System.out.println("| " + b[0] + " | " + b[1] + " | " + b[2] + " |");
+        System.out.println("+---+---+---+");
+        System.out.println("| " + b[3] + " | " + b[4] + " | " + b[5] + " |");
+        System.out.println("+---+---+---+");
+        System.out.println("| " + b[6] + " | " + b[7] + " | " + b[8] + " |");
+        System.out.println("+---+---+---+");
+
 		
     }
 }
