fd620bda87eabbb7b29aa2dad7cc46a4fad99352
ChaytonL1 <42649148+ChaytonL1@users.noreply.github.com>
2019-10-16 09:47:20 -0500

    Looping Array

--- Connect/src/main/CallArray.java
+++ Connect/src/main/CallArray.java
@@ -11,106 +11,176 @@ public class CallArray {
 		        };
 	 public static void DisplayArray()	 
 	   {
-		 int random;
-		
+		int [] slots = {1,2,3,4,5,6,7,8,9}; 
+		int winner = 1;
 	        for (int i = 0; i < array.length; i++) {
 	        	for (int j = 0; j < array[i].length; j++) {
 	        		System.out.print(" " + array[i][j] + " ");
 	        	}
 	        	System.out.println();
 	        }
+	        
 	        ArrayList<String> list = new ArrayList<>();
 	        Scanner input = new Scanner(System.in);
 	        int user; 
 	        System.out.println(" Your turn, where would you like to place your coin? ");
 	        user = input.nextInt();
 	        // Display Choice
+	        while (winner != 0) {
 	        if (user == 1 ) 
-	        {
+	        { if ( slots[0] != 0 ) {
 	        	array[0][0] = 'x';
+	        	slots[0] = 0;
 	        }
-	        if (user == 2 ) 
+	        else
 	        {
+	        	System.out.println(" Slot is already taken.");
+	        }
+	        }
+	        
+	        if (user == 2 ) 
+	        { if ( slots[1] != 0 ) {
 	        	array[0][1] = 'x';
+	        	slots[1] = 0;
+	        }
+	        else
+	        {
+	        	System.out.println(" Slot is already taken.");
+	        }
 	        }
 	        if (user == 3 ) 
-	        {
+	        { if ( slots[2] != 0 ) {
 	        	array[0][2] = 'x';
+	        	slots[2] = 0;
+	        }
+	        else
+	        {
+	        	System.out.println(" Slot is already taken.");
+	        }
 	        }
 	        if (user == 4 ) 
-	        {
+	        { if ( slots[3] != 0 ) {
 	        	array[1][0] = 'x';
+	        	slots[3] = 0;
+	        }
+	        else
+	        {
+	        	System.out.println(" Slot is already taken.");
+	        }
 	        }
 	        if (user == 5 ) 
-	        {
+	        { if ( slots[4] != 0 ) {
 	        	array[1][1] = 'x';
+	        	slots[4] = 0;
+	        }
+	        else
+	        {
+	        	System.out.println(" Slot is already taken.");
+	        }
 	        }
 	        if (user == 6 ) 
-	        {
+	        { if ( slots[5] != 0 ) {
 	        	array[1][2] = 'x';
+	        	slots[5] = 0;
+	        }
+	        else
+	        {
+	        	System.out.println(" Slot is already taken.");
+	        }
 	        }
 	        if (user == 7 ) 
-	        {
+	        { if ( slots[6] != 0 ) {
 	        	array[2][0] = 'x';
+	        	slots[6] = 0;
+	        }
+	        else
+	        {
+	        	System.out.println(" Slot is already taken.");
+	        }
 	        }
 	        if (user == 8 ) 
-	        {
+	        { if ( slots[7] != 0 ) {
 	        	array[2][1] = 'x';
+	        	slots[7] = 0;
+	        }
+	        else
+	        {
+	        	System.out.println(" Slot is already taken.");
+	        }
 	        }
 	        if (user == 9 ) 
-	        {
+	        { if ( slots[8] != 0 ) {
 	        	array[2][2] = 'x';
+	        	slots[8] = 0;
 	        }
-
+	        else
+	        {
+	        	System.out.println(" Slot is already taken.");
+	        }
+	        }
+	        
 	        for (int i = 0; i < array.length; i++) {
 	        	for (int j = 0; j < array[i].length; j++) {
 	        		System.out.print(" " + array[i][j] + " ");
 	        	}
 	        	System.out.println();
-//	    // AI
-//	        	Random rand = new Random();
-//	        	random = rand.nextInt((9) + 1);
-//	        	if (random == 1 ) 
-//		        {
-//		        	array[0][0] = 'x';
-//		        }
-//	        	else if (random == 2 ) 
-//		        {
-//		        	array[0][1] = 'x';
-//		        }
-//	        	else if (random == 3 ) 
-//		        {
-//		        	array[0][2] = 'x';
-//		        }
-//	        	else if (random == 4 ) 
-//		        {
-//		        	array[1][0] = 'x';
-//		        }
-//	        	else if (random == 5 ) 
-//		        {
-//		        	array[1][1] = 'x';
-//		        }
-//	        	else if (random == 6 ) 
-//		        {
-//		        	array[1][2] = 'x';
-//		        }
-//	        	else if (random == 7 ) 
-//		        {
-//		        	array[2][0] = 'x';
-//		        }
-//	        	else if (random == 8 ) 
-//		        {
-//		        	array[2][1] = 'x';
-//		        }
-//	        	else if (random == 9 ) 
-//		        {
-//		        	array[2][2] = 'x';
-//		        }
-//		        for (int a = 0; a < array.length; a++) {
-//		        	for (int b = 0; b < array[a].length; b++) {
-//		        		System.out.print(" " + array[a][b] + " ");
-//		        	}
-	    }
+	        }
 	   }
 	   }
+	        
+	     
+	       
+	        	public static void Computer()
+	        	{
+	    // AI
+	        		int random;
+	        		System.out.println(" The AI's turn... ");
+	        	Random rand = new Random();
+	        	random = rand.nextInt((9) + 1);
+	        	if (random == 1 ) 
+		        {
+		        	array[0][0] = 'o';
+		        }
+	        	else if (random == 2 ) 
+		        {
+		        	array[0][1] = 'o';
+		        }
+	        	else if (random == 3 ) 
+		        {
+		        	array[0][2] = 'o';
+		        }
+	        	else if (random == 4 ) 
+		        {
+		        	array[1][0] = 'o';
+		        }
+	        	else if (random == 5 ) 
+		        {
+		        	array[1][1] = 'o';
+		        }
+	        	else if (random == 6 ) 
+		        {
+		        	array[1][2] = 'o';
+		        }
+	        	else if (random == 7 ) 
+		        {
+		        	array[2][0] = 'o';
+		        }
+	        	else if (random == 8 ) 
+		        {
+		        	array[2][1] = 'o';
+		        }
+	        	else if (random == 9 ) 
+		        {
+		        	array[2][2] = 'o';
+		        }
+		        for (int a = 0; a < array.length; a++) {
+		        	for (int b = 0; b < array[a].length; b++) {
+		        		System.out.print(" " + array[a][b] + " ");
+		   }
+		        	System.out.println(" ");
+       }
+}
+}
+
+
 	   
