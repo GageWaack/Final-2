8aa201d3d5565f21afa9069ef2b0a54b145389e2
GageWaack <45102148+GageWaack@users.noreply.github.com>
2019-10-16 09:08:52 -0500

    Graphics almost done

--- Connect/src/PlainBoard.java
+++ Connect/src/PlainBoard.java
@@ -1,31 +1,39 @@
 public class PlainBoard 
 {
-   public static void displayBoard() {
-    }
-    public static void displayBetterBoard() {
+   public static void displayBoard() 
+   {
+   }
+    public static void displayBetterBoard() 
+    {
     	
     	
-		for(int j =0; j< 15; ++j) {
-		System.out.println("	  			 |			    			|				|");
-	}
-		for (int i = 0; i < 113; ++i) {
+		for(int j =0; j< 15; ++j) 
+		{
+		System.out.println("|	  			 |			    			|				|");
+		}
+		for (int i = 0; i < 113; ++i) 
+		{
 			System.out.print("_");
 		}
-		for (int j = 0; j < 15; ++j ) {
-		System.out.println("	  			 |			    			|				|");
+		for (int j = 0; j < 15; ++j ) 
+		{
+		System.out.println("|	  			 |			    X			|				|");
 		}
-		for (int i = 0; i < 113; ++i) {
+		for (int i = 0; i < 113; ++i) 
+		{
 			System.out.print("_");
 		}
-		for(int j =0; j< 15; ++j) {
-		System.out.println("	  			 |			    			|				|");
-	}
-		for (int i = 0; i < 113; ++i) {
-			System.out.print("_");
+		for(int j =0; j< 15; ++j) 
+		{
+		System.out.println("|	  			 |			    			|				|");
 		}
-
-
-    
+		for (int i = 0; i < 113; ++i) 
+		{
+			System.out.print("_");
+		}				
+		
+			
+		
     }
 }
 
